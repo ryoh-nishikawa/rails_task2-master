77814aa67e481f2dc474fd6ea5dd44f1cea3ec1f
ryoh_nishikawa <ryoh_nishikawa@icloud.com>
2018-06-28 16:04:11 +0900

    app commit

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ApplicationRecord
-    validates:title,presence:true
-    validates :content,length: { in: 1..140 }
+    validates :title,presence:true
+    validates :content,presence:true
 end
